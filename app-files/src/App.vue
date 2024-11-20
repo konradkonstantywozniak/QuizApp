293ea611b413025c5d6b12d10630030a9ea189c7
Konrad <konrad.konstanty.wozniak@gmail.com>
2024-11-20 16:38:50 +0100

    Add compnents and views files / clear old

--- app-files/src/App.vue
+++ app-files/src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <QuizQuestions msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import QuizQuestions from './components/QuizQuestions.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    QuizQuestions
   }
 }
 </script>
